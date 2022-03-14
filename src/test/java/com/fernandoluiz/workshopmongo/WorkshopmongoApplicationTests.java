15c48f2b576c822b61032d361378061f0f531f9b
Fernando <fernando.luiz92@hotmail.com>
2022-03-14 07:46:32 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/fernandoluiz/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandoluiz.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
