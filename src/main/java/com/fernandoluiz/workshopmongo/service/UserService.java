0f06eb031be6f960ba8913db6a798c70750c1efd
Fernando <fernando.luiz92@hotmail.com>
2022-03-16 08:08:07 -0300

    Obtendo um usuário por id

--- src/main/java/com/fernandoluiz/workshopmongo/service/UserService.java
+++ src/main/java/com/fernandoluiz/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.fernandoluiz.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fernandoluiz.workshopmongo.domain.User;
 import com.fernandoluiz.workshopmongo.repository.UserRepository;
+import com.fernandoluiz.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encotrado"));
 	}
 
 }
