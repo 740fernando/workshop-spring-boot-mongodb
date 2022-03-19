cdc0f93641a1cd28f1f3454121d035d07075b82e
Fernando <fernando.luiz92@hotmail.com>
2022-03-19 11:43:17 -0300

    Consulta com vários critérios

--- src/main/java/com/fernandoluiz/workshopmongo/resources/util/URL.java
+++ src/main/java/com/fernandoluiz/workshopmongo/resources/util/URL.java
@@ -2,12 +2,22 @@ package com.fernandoluiz.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
+/**
+ * Responsável por conter os métodos para decodificar parâmetro de URL
+ * 
+ * @author Fernando
+ *
+ */
 
 public class URL {
 
 	/**
-	 * Responsável por receber os parametros da request e decodificar;
+	 * Responsável por receber os parametros do tipo string e decodificar;
 	 * @param text
 	 * @return
 	 */
@@ -16,6 +26,21 @@ public class URL {
 			return URLDecoder.decode(text,"UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	/**
+	 * Responsável por receber os parametros do tipo Date e decodificar;
+	 * @param textDate
+	 * @param defaultValue - Em caso de problemas de conversao da Data, sera atribuido um valor de data padrao 
+	 * @return
+	 */
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
 		}
 	}
 }
